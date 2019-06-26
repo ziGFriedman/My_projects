4d3416877d7ca796489276af58c1508d31702c08
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-26 22:58:08 +0500

    Update Main_quiz.py

--- Quiz/Main_quiz.py
+++ Quiz/Main_quiz.py
@@ -9,24 +9,29 @@ def que_one():
     question = Label(root, text='Висит груша и её нельзя скушать?')
     answer = Entry()
     btn = Button(root, text='Ответить!', command=lambda: game(que_two))
-    question.grid(row=0)
-    answer.grid(row=1)
-    btn.grid(row=2)
+    question.grid()
+    answer.grid()
+    btn.grid()
 
     def game(que_two):
-        if answer.get() == 'Лампочка':
+        if answer.get().lower() == 'лампочка':
             que_two()
         else:
             messagebox.showerror('Ошибка!', 'Попробуй ещё раз!')
 
-
 def que_two():
     question_2 = Label(root, text='Зимой и летом одного цвета?')
     answer_2 = Entry()
-    btn = Button(root, text='Ответить!')
-    question_2.grid(row=0)
-    answer_2.grid(row=1)
-    btn_2.grid(row=2)
+    btn_2 = Button(root, text='Ответить!', command=lambda: game_2(que_two))
+    question_2.grid()
+    answer_2.grid()
+    btn_2.grid()
+
+    def game_2(que_two):
+        if answer_2.get().lower() == 'ёлка':
+            messagebox.showinfo('Победа!', 'Ты молодец!')
+        else:
+            messagebox.showerror('Ошибка!', 'Попробуй ещё раз!')
 
 que_one()
 
