21b9face51a5fea291e7578ef93c69dbc8b6586d
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-26 22:49:56 +0500

    Update Main_quiz.py

--- Quiz/Main_quiz.py
+++ Quiz/Main_quiz.py
@@ -1,7 +1,33 @@
 from tkinter import *
+from tkinter import messagebox
 
 root = Tk()
-root.title("Викторина")
-root.geometry("400x400")
+root.title('Викторина')
+root.geometry('200x300')
+
+def que_one():
+    question = Label(root, text='Висит груша и её нельзя скушать?')
+    answer = Entry()
+    btn = Button(root, text='Ответить!', command=lambda: game(que_two))
+    question.grid(row=0)
+    answer.grid(row=1)
+    btn.grid(row=2)
+
+    def game(que_two):
+        if answer.get() == 'Лампочка':
+            que_two()
+        else:
+            messagebox.showerror('Ошибка!', 'Попробуй ещё раз!')
+
+
+def que_two():
+    question_2 = Label(root, text='Зимой и летом одного цвета?')
+    answer_2 = Entry()
+    btn = Button(root, text='Ответить!')
+    question_2.grid(row=0)
+    answer_2.grid(row=1)
+    btn_2.grid(row=2)
+
+que_one()
 
 root.mainloop()
