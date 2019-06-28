864cb44d4935ddce31bb84a5c8de3536d95ccc52
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-28 09:56:14 +0500

    Update Main_calc.py

--- Calculator/Main_calc.py
+++ Calculator/Main_calc.py
@@ -5,4 +5,44 @@ from tkinter import ttk
 root = Tk()
 root.title('Калькулятор')
 
+# Логика калькулятора
+def calc(key):
+    global memory
+    if key == '=':
+        # исключаем написание букв
+        str1 = '-+0123456789.*/'
+        if calc_entry.get()[0] not in str1:
+            calc_entry.insert(END, 'Первый символ не число!')
+            messagebox.showerror('Ошибка!', 'Вы ввели не число!')
+        # Счёт
+        try:
+            result = eval(calc_entry.get())
+            calc_entry.insert(END, '=' + str(result))
+        except:
+            calc_entry.insert(END, 'Ошибка!')
+            messagebox.showerror('Ошибка!', 'Проверь правильность данных!')
+            
+
+
+# Создаем кнопки
+bttn_list = [
+    '7', '8', '9', '+', '-',
+    '4', '5', '6', '*', '/',
+    '1', '2', '3', '-/+', '='
+    '0', '.', 'C'
+]
+r = 1
+c = 0
+for i in bttn_list:
+    rel = ''
+    cmd=lambda x=i: #calc(x)
+    ttk.Button(root, text=i, command=cmd).grid(row=r, column=c)
+    c += 1
+    if c > 4:
+        c = 0
+        r += 1
+
+calc_entry = Entry(root, width=33)
+calc_entry.grid(row=0, column=0 ,columnspan=5)
+
 root.mainloop()
