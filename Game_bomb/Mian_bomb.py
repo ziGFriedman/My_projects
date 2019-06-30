b01da2d43d13b9a8691e0b9f41d93ca8c5591f79
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-06-30 21:20:21 +0500

    Update Mian_bomb.py

--- Game_bomb/Mian_bomb.py
+++ Game_bomb/Mian_bomb.py
@@ -4,6 +4,12 @@ root = Tk()
 root.title('Бомба')
 root.geometry('500x500')
 
+startLabel = Label(root, text='Нажми Enter, чтобы начать игру!', font=('Helvetica', 12))
+bombLabel = Label(root, text='Фитиль:',font=('Helvetica', 12))
+dayLabel = Label(root, text='День:', font=('Helvetica', 12))
 
+startLabel.pack()
+bombLabel.pack()
+dayLabel.pack()
 
 root.mainloop()
