e29438f6f60e34691182525a9a974345e1d590a0
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-07-03 11:19:43 +0500

    Update Main_notes.py

--- Notes/Main_notes.py
+++ Notes/Main_notes.py
@@ -3,9 +3,21 @@ from tkinter import messagebox
 from tkinter.filedialog import askopenfile
 from tkinter.filedialog import asksaveasfile
 
+file_name = NONE
+
+def new_file():
+    global file_name
+    file_name = 'Без названия'
+    text.delete('1.0', END)
+
+
+
 root = Tk()
 root.title('Заметки')
 root.geometry('400x400')
+
+text = Text(root, width=400, height=400)
+text.pack()
 
 menu_bar = Menu(root)
 file_menu = Menu(menu_bar)
