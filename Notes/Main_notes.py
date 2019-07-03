4449bb5e285ebb31d9f0b820af04e9d1b18fc43d
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-07-03 11:15:45 +0500

    Update Main_notes.py

--- Notes/Main_notes.py
+++ Notes/Main_notes.py
@@ -7,4 +7,10 @@ root = Tk()
 root.title('Заметки')
 root.geometry('400x400')
 
+menu_bar = Menu(root)
+file_menu = Menu(menu_bar)
+menu_bar.add_cascade(label='Файл', menu=file_menu)
+
+root.config(menu=menu_bar)
+
 root.mainloop()
