52c7ab094b651cac8e199024d47b1a6c1adbe9a7
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-07-03 11:27:33 +0500

    Update Main_notes.py

--- Notes/Main_notes.py
+++ Notes/Main_notes.py
@@ -10,6 +10,24 @@ def new_file():
     file_name = 'Без названия'
     text.delete('1.0', END)
 
+def save_as():
+    out = asksaveasfile(mode='w', defaultextension='.txt')
+    data = text.get('1.0', END)
+    try:
+        put.write(data.rstrip())
+    except Exception:
+        messagebox.showerror('Ой!', 'Нельзя сохранить файл!')
+
+def open_file():
+    global file_name
+    inp = askopenfile(mode='r')
+    if inp is None:
+        return
+        file_name = inp.name
+    data = inp.read()
+    text.delete('1.0', END)
+    text.insert('1.0', data)
+    
 
 
 root = Tk()
