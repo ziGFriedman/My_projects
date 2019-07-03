32ce412c60208e18b2d71240a296a83d8940658f
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-07-03 11:13:31 +0500

    Create Main_notes.py

---
+++ Notes/Main_notes.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.filedialog import askopenfile
+from tkinter.filedialog import asksaveasfile
+
+root = Tk()
+root.title('Заметки')
+root.geometry('400x400')
+
+root.mainloop()
