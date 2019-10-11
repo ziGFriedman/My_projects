4a12c1fe5c7ac28df095fb842f76fca1805f4228
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-10-11 22:20:11 +0500

    Update main.py

--- Parser_us-proxy-org/main.py
+++ Parser_us-proxy-org/main.py
@@ -13,3 +13,12 @@ trs = soup.find('table', id='proxylisttable').find('tbody').find_all('tr')
 with open('Parser_us-proxy-org/output.txt', 'w') as file:
     file.write(str(trs))
 file.close()
+
+with open('Parser_us-proxy-org/output.txt', 'r') as file1:
+    str = file1.read()
+    a = tuple(str.split(','))
+    with open('Parser_us-proxy-org/output2.txt', 'w') as file2:
+        for i in a:
+            file2.write(i + '\n')
+    file2.close()
+file1.close()
