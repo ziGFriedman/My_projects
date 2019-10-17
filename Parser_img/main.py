b18a987c38b899b47f77c7dc36cf719e05909d62
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-10-17 17:35:39 +0500

    Create main.py

---
+++ Parser_img/main.py
@@ -0,0 +1,39 @@
+import requests
+
+# url = 'https://www.hdwallpapers.in/download/metal_abstract-HD.jpg'
+#
+# r = requests.get(url, stream=True)    # stream разделить содержимое на куски и закачивает кусками
+#
+# with open('Parser_img/1.jpg', 'wb') as f:
+#     for chunk in r.iter_content(8192):    # указываем сколько байтов будет каждая часть
+#         f.write(chunk)
+
+urls = [
+        'https://www.hdwallpapers.in/download/metal_abstract-HD.jpg',
+        'https://www.hdwallpapers.in/thumbs/2019/ancient_castle_in_winter_snow_4k-t2.jpg',
+        'https://www.hdwallpapers.in/thumbs/2019/spring_anime_girl-t2.jpg',
+        'https://www.hdwallpapers.in/thumbs/2019/marshmello_american_dj_live_5k-t2.jpg',
+        'https://www.hdwallpapers.in/thumbs/2019/horsehead_nebula_radiance_4k-t2.jpg'
+]
+
+def get_file(url):
+    r = requests.get(url, stream=True)
+    return r
+
+def get_name(url):
+    # https://www.hdwallpapers.in/download/metal_abstract-HD.jpg
+    # name: metal_abstract-HD.jpg
+    name = url.split('/')[-1]
+    return name
+
+def save_img(name, file_object):
+    with open('Parser_img/' + name, 'wb') as f:
+        for chunk in file_object.iter_content(8192):
+            f.write(chunk)
+
+def main():
+    for url in urls:
+        save_img(get_name(url), get_file(url))
+
+if __name__ == '__main__':
+    main()
