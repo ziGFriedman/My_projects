7c8bf6eb09a930507a2458aedbf8f1ea4ae62e9e
Nikolay Strizhak <ziGFriedman@gmail.com>
2019-10-21 16:41:03 +0500

    Update main.py

--- Parser_avito/main.py
+++ Parser_avito/main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 def get_html(url):
     r = requests.get(url)
@@ -13,6 +14,43 @@ def get_total_pages(html):
 
     return int(total_pages)
 
+def write_csv(data):
+    with open('Parser_avito/avito.csv', 'a') as f:
+        writer = csv.writer(f)
+
+        writer.writerow( (data['title'],
+                          data['price'],
+                          data['url']) )
+
+def get_page_data(html):
+    soup = BeautifulSoup(html, 'lxml')
+
+    ads = soup.find('div', class_='js-catalog_serp').find_all('div', class_='item item_table clearfix js-catalog-item-enum item-with-contact js-item-extended')
+
+    for ad in ads:
+        # title, price, url
+        try:
+            title = ad.find('div', class_='description item_table-description').find('a').get('title')
+        except:
+            title = ''
+
+        try:
+            price = ad.find('div', class_='description item_table-description').find('div', class_='about').find('span', class_='price').text.strip()
+        except:
+            price = ''
+
+        try:
+            url = 'https://www.avito.ru' + ad.find('div', class_='description item_table-description').find('a').get('href')
+        except:
+            url = ''
+
+        data = {'title': title,
+                'price': price,
+                'url': url}
+
+        write_csv(data)
+
+
 def main():
     url = 'https://www.avito.ru/moskva/telefony/htc?p=1'
     base_url = 'https://www.avito.ru/moskva/telefony/htc?'
@@ -22,7 +60,8 @@ def main():
 
     for i in range(1, total_pages):
         url_gen = base_url + page_part + str(i)
-        
+        html = get_html(url_gen)
+        get_page_data(html)
 
 
 if __name__ == '__main__':
